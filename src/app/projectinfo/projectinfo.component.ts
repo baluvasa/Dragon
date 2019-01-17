2b62e551bd316e436fdc403c4cb39461bb964bf3
Mani Bandari <MB00507120@techmahindra.com>
2019-01-17 18:18:35 +0530

    datatable added

--- src/app/projectinfo/projectinfo.component.ts
+++ src/app/projectinfo/projectinfo.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
+import 'datatables.net'
 @Component({
   selector: 'app-projectinfo',
   templateUrl: './projectinfo.component.html',
   styleUrls: ['./projectinfo.component.scss']
 })
 export class ProjectinfoComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+
+      (<any> $('#example')).DataTable({
+        dom: 'Bfrtip',
+              'paging'      : true,
+              'lengthChange': false,
+              'searching'   : true,
+              'ordering'    : true,
+              'info'        : true,
+              'autoWidth'   : true,
+              'scrollX'     : true,
+               scrollY : '50vh',
+               scrollCollapse: true,
+              /*fixedColumns:   true,*/
+               fixedHeader: {
+                    header: false,
+                    footer: true
+               },
+               lengthMenu: [
+                    [ 10, 20, 50, -1 ],
+                    [ '10 rows', '20 rows', '50 rows', 'Show all' ]
+                ],
+                buttons: [
+                    { extend: 'pageLength',class:'page_length'},
+                    { extend: 'colvis', text: 'Show/Hide Column'},
+                    { extend: 'excel', text: 'Export To Excel' },
+                ]
+      });
   }
 
 }
