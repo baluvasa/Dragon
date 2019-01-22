7b95245f3716a7c2bcbf7a7409ad4dcf60d71ea1
Mani Bandari <MB00507120@techmahindra.com>
2019-01-22 11:55:26 +0530

    added datatable

--- src/app/projectinfo/projectinfo.component.ts
+++ src/app/projectinfo/projectinfo.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import 'datatables.net'
+import 'datatables.net-dt'
+import 'datatables.net-bs'
+
 @Component({
   selector: 'app-projectinfo',
   templateUrl: './projectinfo.component.html',
@@ -12,7 +15,7 @@ export class ProjectinfoComponent implements OnInit {
   ngOnInit() {
 
       (<any> $('#example')).DataTable({
-        dom: 'Bfrtip',
+              dom: 'Bfrtip',
               'paging'      : true,
               'lengthChange': false,
               'searching'   : true,
