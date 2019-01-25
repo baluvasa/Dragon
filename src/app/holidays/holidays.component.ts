594753aa421589f098ec00f739bf15aa5ecd416a
Navvula Prashanthi <NP00585716@techmahindra.com>
2019-01-25 14:36:56 +0530

    holiday validation added

--- src/app/holidays/holidays.component.ts
+++ src/app/holidays/holidays.component.ts
@@ -18,16 +18,27 @@ export class HolidaysComponent implements OnInit {
   project=null;
   edit=0;
   yr=0;
-  jan:any;
-  feb:any;
-  mar:any;
-  Apr:any;
-  may:any;
+  
+  
+  dtOptions: any = {};
   constructor(private formBuilder: FormBuilder) { 
   }
   
   ngOnInit() {
     
+    this.dtOptions = {
+        dom: 'Bfrtip',
+        lengthMenu: [
+            [ 10, 25, 50, -1 ],
+            [ '10 rows', '25 rows', '50 rows', 'Show all' ]
+        ],
+        pagingType: 'full_numbers',
+        buttons: [
+          'pageLength',
+          'colvis',
+          'excel'
+        ]
+      }; 
     this.projects =[
       {projectname:"proj1"},
       {projectname:"proj2"},
@@ -119,5 +130,12 @@ export class HolidaysComponent implements OnInit {
     this.yr=0;
     // this.holidayform.reset(); 
   }
+  saveholidayinfo(){
+    this.edit=0;
+    this.yr=0;
+    this.holidaylistform.reset();
+    alert('Data Added Successfully');
+    
+    }  
  
 }
