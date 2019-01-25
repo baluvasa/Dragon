d2d212bafc63342e4b5062d5f0096685cacd553c
Mani Bandari <MB00507120@techmahindra.com>
2019-01-25 14:37:08 +0530

    Leave Updated

--- src/app/leavedetails/leavedetails.component.ts
+++ src/app/leavedetails/leavedetails.component.ts
@@ -11,9 +11,13 @@ export class LeavedetailsComponent implements OnInit {
   projects:any;
   project_years:any;
   associates:any;
+  leavelists:any;
+  dtOptions: any = {};
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+
     this.projects=[
       {name:'project A'},
       {name:'project B'},
@@ -37,7 +41,20 @@ export class LeavedetailsComponent implements OnInit {
       projectyear:new FormControl('',{}),    
       associatesid:new FormControl('',{})    
     })
-
+   
+    this.dtOptions = {
+        dom: 'Bfrtip',
+        lengthMenu: [
+            [ 10, 25, 50, -1 ],
+            [ '10 rows', '25 rows', '50 rows', 'Show all' ]
+        ],
+        pagingType: 'full_numbers',
+        buttons: [
+          'pageLength',
+          'colvis',
+          'excel'
+        ]
+      };   
   }
   nodata(){
     this.error="No Data Found";
@@ -48,5 +65,16 @@ exception(){
 badrequest(){
     this.error="Bad Request";
 }
-
+searchleaves(){
+  this.leavelists=[
+    {name:'Cara Stevens',position:'Sales Assistant',office:'New York',age:'46',start_date:'2011/12/06',salary:'$145,600'},
+    {name:'Donna Snider',position:'Customer Support',office:'New York',age:'27',start_date:'2011/01/25',salary:'$112,000'},
+    {name:'Hermione Butler',position:'Regional Director',office:'London',age:'47',start_date:'2011/03/21',salary:'$356,250'},
+    {name:'Jennifer Acosta',position:'Junior Javascript Developer',office:'Edinburgh',age:'43',start_date:'2013/02/01',salary:'$75,650'},
+    {name:'Jonas Alexander',position:'Developer',office:'San Francisco',age:'30',start_date:'2010/07/14',salary:'$86,500'},
+    {name:'Lael Greer',position:'Systems Administrator',office:'London',age:'21',start_date:'2009/02/27',salary:'$103,500'},
+    {name:'Michael Bruce',position:'Javascript Developer',office:'Singapore',age:'29',start_date:'2011/06/27',salary:'$183,000'},
+    {name:'Shad Decker',position:'Regional Director',office:'Edinburgh',age:'51',start_date:'2008/11/13',salary:'$183,000'},
+  ];
+}
 }
--- src/app/projectinfo/projectinfo.component.ts
+++ src/app/projectinfo/projectinfo.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
-import { DataTablesModule } from 'angular-datatables';
+
 
 @Component({
   selector: 'app-projectinfo',
