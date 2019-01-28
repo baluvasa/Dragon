858d6531d744abc089bd23817b010c94753350ed
Syed Imran Akhil <SA00587670@techmahindra.com>
2019-01-28 19:47:21 +0530

    Account Category-updated

--- src/app/accountcategory/accountcategory.component.ts
+++ src/app/accountcategory/accountcategory.component.ts
@@ -23,10 +23,12 @@ export class AccountcategoryComponent implements OnInit  {
     }
 
     this.projects=[
-      {name:'INR'},
-      {name:'USD'},
-      {name:'JPY'},
-      {name:'IDR'}
+      {name:'GE Industrial'},
+      {name:'GE INDIA EXPORTS PVT LTD'},
+      {name:'GE Oil & Gas'},
+      {name:'GE P&W'},
+      {name:'GE Packaged Power'},
+      {name:'GE Ticketless'}
     ];
     this.project_years=[
       {year_month:'10'},
@@ -76,14 +78,14 @@ badrequest(){
 }
 searchleaves(){
   this.leavelists=[
-    {sno:'1',projectname:'INR',associateid:'10-Jan-18',associatename:'10'},
-    {sno:'2',projectname:'USD',associateid:'12-Jan-16',associatename:'20'},
-    {sno:'3',projectname:'JPY',associateid:'17-Jan-15',associatename:'30'},
-    {sno:'4',projectname:'INR',associateid:'21-Jan-15',associatename:'40'},
-    {sno:'5',projectname:'USD',associateid:'25-Jan-19',associatename:'15'},
-    {sno:'6',projectname:'JPY',associateid:'09-Jan-09',associatename:'25'},
-    {sno:'7',projectname:'INR',associateid:'18-Jan-12',associatename:'35'},
-    {sno:'8',projectname:'USD',associateid:'22-Jan-14',associatename:'45'},
+    {sno:'1',projectname:' GE Industrial-A',associateid:'GE Ticketless',associatename:'10'},
+    {sno:'2',projectname:' GE Appliances',associateid:'GE INDIA EXPORTS PVT LTD',associatename:'20'},
+    {sno:'3',projectname:' GE Industrial',associateid:'GE Oil & Gas ',associatename:'30'},
+    {sno:'4',projectname:' GE Industrial-B',associateid:'GE Packaged Power',associatename:'40'},
+    {sno:'5',projectname:' GE P&W-A',associateid:'Metso',associatename:'15'},
+    {sno:'6',projectname:' GE Industrial',associateid:'GE Amphenol',associatename:'25'},
+    {sno:'7',projectname:' GE Industrial-C',associateid:'GE P&W11',associatename:'35'},
+    {sno:'8',projectname:' GE P&W-B',associateid:'GE APP',associatename:'45'},
   ];
 }
 deletedata(){
