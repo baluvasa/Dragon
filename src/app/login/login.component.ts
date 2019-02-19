401237bb10b6181f0e6c8b241abc8d1184810851
Syed Imran Akhil <SA00587670@techmahindra.com>
2019-02-19 12:05:00 +0530

    lOGIN INTEGRATION

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import { FormBuilder, FormGroup, Validators,FormControl  } from '@angular/forms';
 import { AppServicesService} from '../app-services.service';
+// import { HttpBackend } from '@angular/common/http';
+import { AppLink } from '../app-link';
+// import { url } from 'inspector';
+import { HttpClient } from  "@angular/common/http";
 
 @Component({
     selector: 'app-login',
@@ -10,14 +14,20 @@ import { AppServicesService} from '../app-services.service';
 })
 export class LoginComponent implements OnInit {
     loginform: FormGroup;
+    // ip=AppLink.baseURL;
     error:any;
-    errorinfo:any;    
-    constructor(private formBuilder: FormBuilder,private router:Router,private appservice:AppServicesService) { }
+    errorinfo:any; 
+    logres:any;  
+    msg:any; 
+    afterlogin:any;
+    user_login:any;
+    alogin:any;
+    constructor(private formBuilder: FormBuilder,private router:Router,private appservice:AppServicesService,private  httpClient:HttpClient) { }
     
     ngOnInit() {     
         this.loginform = new FormGroup({
-            username:new FormControl('',{
-              validators: [Validators.required,<any>Validators.minLength(4)],
+            associateId:new FormControl('',{
+              validators: [Validators.required,<any>Validators.minLength(4), <any>Validators.pattern('^[a-zA-Z0-9][a-zA-Z0-9 ]*[a-zA-Z0-9_]$')],
               updateOn:'blur'
             }),
             password:new FormControl('',{
@@ -36,21 +46,30 @@ export class LoginComponent implements OnInit {
         this.error="Bad Request";
     }
     
-    onSubmit(value:any) {
-console.log(value);
-        if(value.username=='admin' && value.password=='admin123'){
-            this.appservice.setLogedinfo('admin');            
-            this.router.navigate(['home']);
-
-        } else if(value.username=='user' && value.password=='user123'){
-            this.appservice.setLogedinfo('user');
-            this.router.navigate(['home']);
-        } else{           
-            this.error='Incorrect Username/password'; 
-            this.loginform.reset();                         
+    onsignin(user_login){
+        
+       
+     this.user_login={associateId:'BV00586358',
+    password:'PO@123456'}
+        let url='http://10.56.67.9:8082/po/login/associate';
+        this.httpClient.post(url,this.user_login).subscribe(result => {
+        console.log("------------",result);
+        this.afterlogin=result;
+        console.log("11111111111111",this.afterlogin.accessDetails.accessType);
+        if(this.afterlogin.accessDetails.accessType=='admin'){
+          this.appservice.setLogedinfo('admin'); 
+          this.router.navigate(['home']);
         }
+        else{
+         
+          this.router.navigate(['profile']);
+          
+        }
+        },
+        error => {
+          this.error = 'Incorrect Username/password'; 
+        });
+      } 
+
     }
-    
-}
-
-
+  
